f212ecae91b4aa6078ffe0e4f783dccb27ee3e35
Hung <longvanleu69@gmail.com>
2023-03-13 19:04:57 +0700

    add test entity, connect into cloudinary

--- src/main/java/com/englishweb/backend/controller/RoleController.java
+++ src/main/java/com/englishweb/backend/controller/RoleController.java
@@ -1,5 +1,6 @@
 package com.englishweb.backend.controller;
 
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,25 +9,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import com.englishweb.backend.entity.User;
-import com.englishweb.backend.repository.UserRepository;
+import com.englishweb.backend.entity.Role;
+import com.englishweb.backend.repository.RoleRepository;
 
 @RestController
 @CrossOrigin
-public class UserController {
-
+public class RoleController {
+	
 
 	@Autowired
-	private UserRepository userRepository;
-	
-	@PostMapping("/user")
-	User newUser(@RequestBody User newUser){
-		return userRepository.save(newUser);
+	private RoleRepository roleRepository;
+
+	@PostMapping("/role")
+	Role newRole(@RequestBody Role newRole) {
+		return roleRepository.save(newRole) ;
 	}
 	
-	@GetMapping("/user/list")
-	public List<User> getAllUser() {
-		return userRepository.findAll();
+	@GetMapping("/role/list")
+	public List<Role> getAllUser() {
+		return roleRepository.findAll();
 	}
-	
+
 }
