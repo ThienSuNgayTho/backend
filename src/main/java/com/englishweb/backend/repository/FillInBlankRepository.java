545c961d51300c504547149e10f1b59e9a8e19c3
Huy <97140326+cut113@users.noreply.github.com>
2023-03-15 09:36:38 +0700

    Done fill in blank pt1

--- src/main/java/com/englishweb/backend/repository/FillInBlankRepository.java
+++ src/main/java/com/englishweb/backend/repository/FillInBlankRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface FillInBlankRepository extends JpaRepository<FillInBlank, Long> {
-    @Query(value = "SELECT * FROM fill_in_blank WHERE levelid = ?1", nativeQuery = true)
+    @Query(value = "select * from fill_in_blank where levelid =?1 order by rand() limit 5 ", nativeQuery = true)
     List<FillInBlank> findAllByLevelId(Long levelid);
 }
