b93fca8615ba370b82923c1b9631769ff18e389c
Huy <97140326+cut113@users.noreply.github.com>
2023-04-02 17:35:05 +0700

    Add lesson to hungle

--- src/main/java/com/englishweb/backend/service/SecurityConfig.java
+++ src/main/java/com/englishweb/backend/service/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeHttpRequests().requestMatchers("/api/auth/*").permitAll()
                 .requestMatchers("/api/test/*").permitAll()
-                .anyRequest().permitAll();
+                .anyRequest().authenticated();
 
         http.authenticationProvider(authenticationProvider());
 
